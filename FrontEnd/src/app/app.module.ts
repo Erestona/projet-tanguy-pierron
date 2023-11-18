124f21d4d6a4bc8ef636e23a2c5acc4ab48abfcd
Tanguy <pierrontanguy@gmail.com>
2023-11-18 08:28:58 +0100

    Added cart module

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppComponent } from './app.component';
@@ -6,14 +7,24 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductSearchComponent } from './product-search/product-search.component';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { LinksComponent } from './links/links.component';
+import { CartComponent } from './cart/cart.component';
+
+const appRoutes : Routes= [
+  {path : 'shop', component : ProductListComponent},
+  {path : 'cart', component : CartComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductSearchComponent, 
+    LinksComponent, 
+    CartComponent, 
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpClientModule,
     FormsModule,
